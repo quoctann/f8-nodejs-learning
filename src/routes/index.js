e007c0e7e4b5725316264c0471f316e3e76ddf91
Quoc Tan <tqtwork@outlook.com>
2021-05-25 13:32:09 +0700

    Test husky git hooks

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 // File này sẽ được đọc sau src/index.js vì để định tuyến, từ file này sẽ sử dụng các file khác trong thư mục routes
-const newsRouter = require('./news');
-const siteRouter = require('./site');
+        const newsRouter = require('./news');
+        const siteRouter = require('./site');
 
 function route(app) {
     // Hệ thống sẽ match đường dẫn URL request từ trên xuống, match cái nào nó sẽ vào cái đó, vd trang chủ với URL '/' sẽ không match được với news mà match xuống '/'
